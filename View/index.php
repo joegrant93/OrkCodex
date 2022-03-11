c0b13617ca8bb01b25a4c384f768cf533749b0fc
Joe Grant <joegrant93@hotmail.com>
2022-03-11 00:22:21 +0000

    Basic Skeleton

--- View/index.php
+++ View/index.php
@@ -1,2 +1,10 @@
-<?php
-echo 'Hello World!';
+<!doctype html>
+<html lang="en">
+<?php include 'Includes/head.php'; ?>
+
+<body>
+    <h1>Hello, world!</h1>
+    <?php include 'Includes/footer.php'; ?>
+</body>
+
+</html>

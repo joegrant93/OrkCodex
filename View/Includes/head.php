be2b3b732dec4f933dc59440cf1f355eeeaaeb1c
Joe Grant <joegrant93@hotmail.com>
2022-03-11 03:59:57 +0000

    .

--- View/Includes/head.php
+++ View/Includes/head.php
@@ -1,6 +1,54 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css">
     <title>Hello, world!</title>
 </head>
+<style>
+
+
+    /*********************************************************************************** */
+    .card-body {
+        visibility: hidden;
+        opacity: 0;
+        transition: visibility 0s, opacity 0.5s linear;
+    }
+
+
+    .card {
+        border: none;
+        transition: all 500ms cubic-bezier(0.19, 1, 0.22, 1);
+        overflow: hidden;
+        border-radius: 20px;
+        min-height: 450px;
+        box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
+
+
+    }
+
+    .card:hover .card-body {
+        visibility: visible;
+        opacity: 1;
+    }
+
+    .card:hover h4{
+        background-color: black;
+    }
+
+
+    @media (max-width: 768px) {
+        .card {
+            min-height: 350px;
+        }
+
+    }
+
+    @media (max-width: 420px) {
+        .card {
+            min-height: 300px;
+        }
+
+    }
+</style>
--- View/index.php
+++ View/index.php
@@ -3,7 +3,31 @@
 <?php include 'includes/head.php'; ?>
 
 <body>
-    <h1>Hello, world!</h1>
+    <?php include 'includes/navigation.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 my-1">
+                <div class="card" style="background-image:url('orkTroopsCard.jpg');">
+                    <img class="card-img d-none" src="orkTroopsCard.jpg">
+                </div>
+            </div>
+            <div class="col-md-4 my-1">
+                <div class="card" style="background-image:url('orkTroopsCard.jpg');">
+                    <img class="card-img d-none" src="orkTroopsCard.jpg">
+                </div>
+            </div>
+            <div class="col-md-4 my-1">
+                <div class="card" style="background-image:url('orkTroopsCard.jpg');">
+                    <img class="card-img d-none" src="orkTroopsCard.jpg">
+                </div>
+            </div>
+            <div class="col-md-4 my-1">
+                <div class="card" style="background-image:url('orkTroopsCard.jpg');">
+                    <img class="card-img d-none" src="orkTroopsCard.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include 'includes/footer.php'; ?>
 </body>
 
